04e370503f215b8e2101f3fee976bcbeb2d0562d
Dahavid17 <davi.a.santos25@aluno.senai.br>
2026-04-22 14:13:03 -0300

    22/04/2026

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-// Perfil de usuario com foto, nome, email, numero, endereço e avaliação
-
 void main() {
   runApp(const MyApp());
 }
@@ -11,149 +9,171 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PerfilUsuario(),
+      title: "Carteira Digital",
+      home: const CarteiraDigital(),
     );
   }
 }
 
-class PerfilUsuario extends StatefulWidget {
-  const PerfilUsuario({super.key});
-
-  @override
-  State<PerfilUsuario> createState() => _PerfilUsuarioState();
-}
-
-class _PerfilUsuarioState extends State<PerfilUsuario> {
-  int avaliacao = 0;
-
-  Widget buildStar(int index) {
-    return IconButton(
-      onPressed: () {
-        setState(() {
-          avaliacao = index;
-        });
-      },
-      icon: Icon(
-        index <= avaliacao ? Icons.star : Icons.star_border,
-        color: Colors.amber,
-      ),
-    );
-  }
+class CarteiraDigital extends StatelessWidget {
+  const CarteiraDigital({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(15),
+      appBar: AppBar(
+        title: const Text("Carteira Digital"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: const [
+          CartaoBanco(
+            corCartao: Colors.deepPurple,
+            banco: "Banco SESI",
+            numero: "1234 5678 9012 3456",
+            nome: "Davi Antonio",
+            validade: "12/30",
+            bandeira: "visa",
           ),
+          SizedBox(height: 20),
 
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+          CartaoBanco(
+            corCartao: Colors.blue,
+            banco: "Banco Digital",
+            numero: "9876 5432 1098 7654",
+            nome: "Davi Santos",
+            validade: "08/28",
+            bandeira: "mastercard",
+          ),
+          SizedBox(height: 20),
+
+          CartaoBanco(
+            corCartao: Colors.green,
+            banco: "Banco XPTO",
+            numero: "1111 2222 3333 4444",
+            nome: "Davi A. Santos",
+            validade: "05/27",
+            bandeira: "outro",
+          ),
+          SizedBox(height: 20),
+
+          CartaoBanco(
+            corCartao: Colors.orange,
+            banco: "Banco Inter",
+            numero: "2222 3333 4444 5555",
+            nome: "Davi Santos",
+            validade: "09/29",
+            bandeira: "mastercard",
+          ),
+          SizedBox(height: 20),
+
+          CartaoBanco(
+            corCartao: Colors.black,
+            banco: "Banco Premium",
+            numero: "9999 8888 7777 6666",
+            nome: "Davi A Santos",
+            validade: "03/31",
+            bandeira: "visa",
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
+
+class CartaoBanco extends StatelessWidget {
+  final Color corCartao;
+  final String banco;
+  final String numero;
+  final String nome;
+  final String validade;
+  final String bandeira;
+
+  const CartaoBanco({
+    super.key,
+    required this.corCartao,
+    required this.banco,
+    required this.numero,
+    required this.nome,
+    required this.validade,
+    required this.bandeira,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // 🔥 imagem LOCAL (do PC/projeto)
+    Widget imagemBandeira;
+
+    if (bandeira == "visa") {
+      imagemBandeira = Image.asset(
+        'assets/images/visa.png',
+        width: 50,
+      );
+    } else if (bandeira == "mastercard") {
+      imagemBandeira = Image.asset(
+        'assets/images/mastercard.png',
+        width: 50,
+      );
+      }else {
+        imagemBandeira = const Icon(
+          Icons.credit_card,
+          color: Colors.white,
+        );
+      }
+
+    return Container(
+      height: 200,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: corCartao,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black54,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
             children: [
-              const CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage(
-                  'assets/images/Design sem nome (4).png',
-                ),
-              ),
+              Text(banco, style: const TextStyle(color: Colors.white)),
+              const Spacer(),
 
-              const SizedBox(height: 10),
+              imagemBandeira,
 
-              const Text(
-                'Nome do Usuário',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 5),
-
-              const Text(
-                'Desenvolvedor JR',
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-
-              const SizedBox(height: 10),
-
-              // 📧 Email
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.email, size: 18),
-                  SizedBox(width: 5),
-                  Text('Email do Usuário'),
-                ],
-              ),
-
-              const SizedBox(height: 5),
-
-              // 📞 Telefone
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.phone, size: 18),
-                  SizedBox(width: 5),
-                  Text('Número do Usuário'),
-                ],
-              ),
-
-              const SizedBox(height: 5),
-
-              // 📍 Endereço
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(Icons.location_on, size: 18),
-                  SizedBox(width: 5),
-                  Text('Endereço do Usuário'),
-                ],
-              ),
-
-              const SizedBox(height: 15),
-
-              const Text('Avaliação do Usuário'),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildStar(1),
-                  buildStar(2),
-                  buildStar(3),
-                  buildStar(4),
-                  buildStar(5),
-                ],
-              ),
-
-              const SizedBox(height: 15),
-
-              // 🔘 Botão dentro do container
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    print('Editar perfil clicado');
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    'Editar Perfil',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ),
+              const Spacer(),
+              const Icon(Icons.contactless, size: 20, color: Colors.white),
             ],
           ),
-        ),
+
+          const Icon(Icons.sim_card, size: 40, color: Colors.amber),
+
+          Text(
+            numero,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 22,
+              letterSpacing: 2,
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(nome, style: const TextStyle(color: Colors.white)),
+              Text(validade, style: const TextStyle(color: Colors.white)),
+            ],
+          ),
+        ],
       ),
     );
   }
